f8edca7d25dd8e93b7cbc871581469bba601700c
NakhunHost <presidentialrealmsstudio@gmail.com>
2024-12-18 21:46:23 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 ### config file ###
-token = "" # Put your discord token here.
+token = "WzExOTg5NTU4MTgzNTYzMTQxNjUsIjEuNDcuMTkzLjE3OSJd.Z2LafQ.0lytCSvpUNrH7XwMATaRdf41m8E" # Put your discord token here.
 preifx = ";" # Put the prefix you want here.
 wait_time = "7460" # Put the wait time in seconds. !!!Dont change this unless you know what you're doing.!!!
 disboard_prefix = "!d" # Put the disboard prefix here no spaces.
